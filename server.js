f6bfb043988e8a37add961f811a3ecc2afe5f07a
SamHoltz <holtzman.samuel1@gmail.com>
2018-09-17 15:19:17 -0500

    final

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const exphbs = require('express-handlebars');
 const connection = require("./config/connection")
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
